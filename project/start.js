19dd15f3f1816f3140eaa94663510b161bf519aa
David Gutla <dgutla@hubspot.com>
2020-01-31 17:27:54 +1100

    yolo

--- project/start.js
+++ project/start.js
@@ -13,12 +13,8 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/', function(request, response) {
+app.get('/about', function(request, response) {
   response.render('pages/about');
-});
-
-app.get('/', function(request, response) {
-  response.render('pages/projects');
 });
 
 app.listen(app.get('port'), function() {
